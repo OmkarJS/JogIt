69773dbc2c41e183545202ce42a4bd5191ec8cf9
Omkar J Suvarna <omkarjsuvarna@gmail.com>
2025-11-23 00:56:11 +0530

    Changes: - Viewmodel blocks added for one instance view models.

--- composeApp/src/commonMain/kotlin/omkar/android/projects/di/AppModule.kt
+++ composeApp/src/commonMain/kotlin/omkar/android/projects/di/AppModule.kt
@@ -1,33 +1,47 @@
 package omkar.android.projects.di
 
-
-import omkar.android.projects.data.local.repository.NotesJoggerRepositoryImpl
+import omkar.android.projects.data.local.repository.notesjogger.NotesJoggerRepositoryImpl
+import omkar.android.projects.shared.password.data.local.repository.PasswordRepositoryImpl
 import omkar.android.projects.data.remote.ExampleClient
 import omkar.android.projects.data.remote.HttpClientEngine
-import omkar.android.projects.domain.repository.NotesJoggerRepository
-import omkar.android.projects.domain.usecases.CreateNotesUseCase
-import omkar.android.projects.domain.usecases.DeleteNoteUseCase
-import omkar.android.projects.domain.usecases.GetNoteFromIDUseCase
-import omkar.android.projects.domain.usecases.GetNotesListUseCase
-import omkar.android.projects.domain.usecases.JogUseCases
-import omkar.android.projects.domain.usecases.UpdateNotesUseCase
+import omkar.android.projects.domain.repository.notesjogger.NotesJoggerRepository
+import omkar.android.projects.shared.password.domain.repository.PasswordRepository
+import omkar.android.projects.domain.usecases.notesjogger.CreateNotesUseCase
+import omkar.android.projects.domain.usecases.notesjogger.DeleteNoteUseCase
+import omkar.android.projects.domain.usecases.notesjogger.GetNoteFromIDUseCase
+import omkar.android.projects.domain.usecases.notesjogger.GetNotesListUseCase
+import omkar.android.projects.domain.usecases.notesjogger.JogUseCases
+import omkar.android.projects.domain.usecases.notesjogger.UpdateNotesUseCase
+import omkar.android.projects.shared.password.domain.usecases.GenerateSaltUseCase
+import omkar.android.projects.shared.password.domain.usecases.HashPasswordWithSaltUseCase
+import omkar.android.projects.shared.password.domain.usecases.HashPasswordWithoutSaltUseCase
+import omkar.android.projects.shared.password.domain.usecases.PasswordUseCases
+import omkar.android.projects.shared.password.domain.usecases.VerifyPasswordUseCase
 import omkar.android.projects.presentation.home.HomeViewModel
 import omkar.android.projects.presentation.jogdetails.JogDetailsViewModel
+import omkar.android.projects.shared.password.PasswordManager
+import omkar.android.projects.shared.password.presentation.PasswordViewmodel
 import org.koin.dsl.module
+import org.koin.compose.viewmodel.dsl.viewModel
 
 val commonModule = module {
-    /**  Repository  */
-    single<NotesJoggerRepository> { NotesJoggerRepositoryImpl(get()) }
+    /**  Managers  */
+    single<PasswordManager> { PasswordManager() }
 
+    /**  Repository  */
+    // Notes jogger
+    single<NotesJoggerRepository> { NotesJoggerRepositoryImpl(get()) }
+    // Password
+    single<PasswordRepository> { PasswordRepositoryImpl(get()) }
 
     /**  Usecase  */
-    // Joggable
-    factory { CreateNotesUseCase(get()) }
-    factory { UpdateNotesUseCase(get()) }
-    factory { GetNotesListUseCase(get()) }
-    factory { GetNoteFromIDUseCase(get()) }
-    factory { DeleteNoteUseCase(get()) }
-    factory {
+    //  Notes jogger
+    single { CreateNotesUseCase(get()) }
+    single { UpdateNotesUseCase(get()) }
+    single { GetNotesListUseCase(get()) }
+    single { GetNoteFromIDUseCase(get()) }
+    single { DeleteNoteUseCase(get()) }
+    single {
         JogUseCases(
             createNotesUseCase = get(),
             updateNotesUseCase = get(),
@@ -37,6 +51,19 @@ val commonModule = module {
         )
     }
 
+    // Password
+    single { HashPasswordWithoutSaltUseCase(get()) }
+    single { HashPasswordWithSaltUseCase(get()) }
+    single { GenerateSaltUseCase(get()) }
+    single { VerifyPasswordUseCase(get()) }
+    single {
+        PasswordUseCases(
+            hashPasswordWithoutSaltUseCase = get(),
+            hashPasswordWithSaltUseCase = get(),
+            generateSaltUseCase = get(),
+            verifyPasswordUseCase = get()
+        )
+    }
 
     /**  Client  */
     val httpClient = HttpClientEngine().create()
@@ -48,12 +75,17 @@ val commonModule = module {
         HomeViewModel(get())
     }
 
-    factory {
-        // Returns new viewmodel instance every time this is called. Add if needed.
+    viewModel {
         JogDetailsViewModel(
             createNotesUseCase = get(),
             updateNotesUseCase = get(),
             getNoteFromIDUseCase = get()
+        )
+    }
+
+    viewModel {
+        PasswordViewmodel(
+            passwordUseCases = get()
         )
     }
 }
