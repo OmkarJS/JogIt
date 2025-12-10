d0b3a6bb78f387afbc194eaefa6d6986bbb5314f
Omkar J Suvarna <omkarjsuvarna@gmail.com>
2025-12-10 22:25:00 +0530

    Changes: - Home screen UI fix.

--- composeApp/src/commonMain/kotlin/omkar/android/projects/presentation/home/HomePage.kt
+++ composeApp/src/commonMain/kotlin/omkar/android/projects/presentation/home/HomePage.kt
@@ -1,24 +1,31 @@
 package omkar.android.projects.presentation.home
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.selection.LocalTextSelectionColors
 import androidx.compose.material.Card
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -28,6 +35,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -35,11 +43,13 @@ import androidx.compose.ui.unit.dp
 import jogit.composeapp.generated.resources.Res
 import jogit.composeapp.generated.resources.compose_multiplatform
 import omkar.android.projects.LocalAppColors
+import omkar.android.projects.app.components.DefaultSpacer
 import omkar.android.projects.app.components.ExtraSmallSpacer
 import omkar.android.projects.app.components.MediumText
 import omkar.android.projects.app.components.SemiLargeText
 import omkar.android.projects.app.components.SmallSpacer
 import omkar.android.projects.app.components.TextConfig
+import omkar.android.projects.app.components.percentOfScreenHeight
 import omkar.android.projects.app.widget.HomeRoofView
 import omkar.android.projects.app.widget.bottomsheet.PasswordUnlockSheet
 import omkar.android.projects.app.widget.icon.CustomIcon
@@ -148,6 +158,7 @@ fun HomePage(
                 items(notesList, key = { it.id }) { note ->
                     NoteItem(
                         note = note,
+                        isProtected = passwordViewModel.isProtected(note),
                         onClick = {
                             if(note.hasBiometricLock) {
                                 passwordViewModel.authenticateFingerprint()
@@ -196,51 +207,87 @@ fun HomePage(
 @Composable
 fun NoteItem(
     note: Joggable,
+    isProtected: Boolean,
     onClick: () -> Unit,
     onDelete: (Joggable) -> Unit
 ) {
-    Card(
+    Box(
         modifier = Modifier
             .fillMaxWidth()
-            .clickable { onClick() },
-        shape = RoundedCornerShape(12.dp)
+            .clip(RoundedCornerShape(12.dp))
+            .background(LocalAppColors.current.surface)
+            .clickable {
+                onClick()
+            }
     ) {
         Row(
             modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp),
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 20.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
             Column(modifier = Modifier.weight(1f)) {
-                SemiLargeText(
-                    text = note.title,
-                    TextConfig(
-                        fontWeight = FontWeight.SemiBold
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    SemiLargeText(
+                        text = note.title,
+                        TextConfig(
+                            fontWeight = FontWeight.SemiBold
+                        )
                     )
-                )
+
+                    DefaultSpacer(horizontal = true)
+
+                    if (isProtected) {
+                        CustomIcon(
+                            icon = Icons.Default.Lock,
+                            iconSize = 14.dp,
+                            iconColor = LocalAppColors.current.grey.copy(0.6f)
+                        )
+                    }
+                }
 
                 ExtraSmallSpacer()
 
-                MediumText(
-                    text = note.content,
-                    TextConfig(
-                        maxLines = 2,
-                        overflow = TextOverflow.Ellipsis
+                if (isProtected) {
+                    Column {
+                        SmallSpacer()
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth(0.9f)
+                                .height(percentOfScreenHeight(1))
+                                .clip(RoundedCornerShape(4.dp))
+                                .background(Color.LightGray.copy(alpha = 0.6f))
+                        )
+                        ExtraSmallSpacer()
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth(0.7f)
+                                .height(percentOfScreenHeight(1))
+                                .clip(RoundedCornerShape(4.dp))
+                                .background(Color.LightGray.copy(alpha = 0.6f))
+                        )
+                    }
+                } else {
+                    MediumText(
+                        text = note.content,
+                        TextConfig(
+                            maxLines = 2,
+                            overflow = TextOverflow.Ellipsis
+                        )
                     )
-                )
+                }
             }
 
             Spacer(modifier = Modifier.width(8.dp))
 
-            Icon(
-                painter = painterResource(Res.drawable.compose_multiplatform),
-                modifier = Modifier
-                    .padding(6.dp)
-                    .size(20.dp)
-                    .clickable {
-                        onDelete(note)
-                    },
-                contentDescription = "Delete"
+            CustomIcon(
+                icon = Icons.Default.Delete,
+                iconSize = 25.dp,
+                onClick = {
+                    onDelete(note)
+                },
+                modifier = Modifier.padding(6.dp),
+                iconColor = LocalAppColors.current.primary
             )
         }
     }
